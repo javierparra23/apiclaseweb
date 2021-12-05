fc8f8fa3d7d792cb61861259b7ed42703a3a9770
MichaelMejia683 <63168301+MichaelMejia683@users.noreply.github.com>
2021-12-04 21:41:48 -0500

    Agregue dos POST

--- index.js
+++ index.js
@@ -10,4 +10,18 @@
         console.log("Conectado puerto 3000")
     } )
 
+    app.post('/maicol',(req,res) => {
+        res.json({
+        "cedula" : "455555",
+        "nombre" : "maicol"
+        })
+    })
+
+    app.post('/michel',(req,res) => {
+        res.json({
+        "cedula" : "1554273286286",
+        "nombre" : "michel"
+        })
+    })
+
 
